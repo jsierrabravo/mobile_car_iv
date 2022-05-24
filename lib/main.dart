25597405288ba6241e47fbe7f2efc13f6753cbbc
jsierrabravo <jsierra@lsv-tech.com>
2022-05-24 18:05:45 -0500

    Added up and down counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:myfirstproject/examples.dart';
-import 'package:myfirstproject/view/home_view.dart';
+import 'package:myfirstproject/view/counter.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp ({Key? key}) : super(key: key);
+  
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeView(),
+      home: Counter(),
     ); 
   }
 }
---
+++ lib/view/counter.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class Counter extends StatefulWidget {
+  const Counter ({Key? key}) : super(key: key);
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+
+  int _counter = 0;
+  
+  @override
+  Widget build(BuildContext context) {
+
+    const titleFontSize = 30.0;
+    const paragraphFontSize = 25.0;
+
+    return Scaffold(
+
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 255, 99, 71),
+        title: const Text('Counter'),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "Number of taps",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: titleFontSize,
+              ),
+            ),
+            Text(
+              '$_counter',
+              style: const TextStyle(
+                fontSize: paragraphFontSize,
+              ),
+            ),
+          ]
+        )
+      ),
+      
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => setState(() => _counter++),
+            backgroundColor: Colors.green,
+            child: const Icon(Icons.arrow_circle_up_outlined),
+            tooltip: 'Increment Counter',
+          ),
+          const SizedBox(width: 15),
+          FloatingActionButton(
+            onPressed: () => setState(() => _counter--),
+            backgroundColor: Colors.red,
+            child: const Icon(Icons.arrow_circle_down_rounded),
+            tooltip: 'Decrement Counter',
+          )
+        ],
+      ),
+
+    );
+  }
+}
